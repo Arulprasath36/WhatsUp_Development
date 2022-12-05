4dc6d4543cb86b99dc174fcb13b3a9a0d4257c22
Durairaji <112107468+Durairaji@users.noreply.github.com>
2022-12-05 19:23:37 +0530

    simplepratise

--- ClassDemo.java
+++ ClassDemo.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -25,8 +26,8 @@ public class ClassDemo {
 		}
 	@BeforeMethod
 	public void browserLaunch() {
-		driver.get("https://www.facebook.com/campaign/landing.php?campaign_id=14884913640&extra_1=s%7Cc%7C550525805640%7Cb%7Cfb%20create%20account%7C&placement=&creative=550525805640&keyword=fb%20create%20account&partner_id=googlesem&extra_2=campaignid%3D14884913640%26adgroupid%3D128696221432%26matchtype%3Db%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-332264388364%26loc_physical_ms%3D1007809%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=CjwKCAiAp7GcBhA0EiwA9U0mthjBxl43F-J6cdpIUnSCYQxvXF2DXpPNWzJ624RVPJ9NhRCEiQcHQhoCwKkQAvD_BwE");
-		
+	driver.get("https://www.facebook.com/campaign/landing.php?campaign_id=14884913640&extra_1=s%7Cc%7C550525805640%7Cb%7Cfb%20create%20account%7C&placement=&creative=550525805640&keyword=fb%20create%20account&partner_id=googlesem&extra_2=campaignid%3D14884913640%26adgroupid%3D128696221432%26matchtype%3Db%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-332264388364%26loc_physical_ms%3D1007809%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=CjwKCAiAp7GcBhA0EiwA9U0mthjBxl43F-J6cdpIUnSCYQxvXF2DXpPNWzJ624RVPJ9NhRCEiQcHQhoCwKkQAvD_BwE");	
+	
 	}
 	@Test()
 	public void testCase1() throws InterruptedException {
@@ -55,9 +56,22 @@ public class ClassDemo {
 	@Test()
 	public void testCase5() throws InterruptedException {
 		Thread.sleep(3000);
-		//driver.findElement(By.xpath("//select[@aria-label='Day']")).sendKeys("16");
-		WebElement findElement = driver.findElement(By.xpath("//select[@aria-label='Day']"));
-		findElement.sendKeys("16",Keys.ENTER);
+		WebElement dropdown = driver.findElement(By.xpath("(//select[contains(@class,'_9407 _5dba')])[1]"));
+		Select s=new Select(dropdown);
+		s.selectByVisibleText("4");
+		
+	}
+	@Test
+	private void testCase6() {
+		WebElement dropdown2 = driver.findElement(By.xpath("(//select[contains(@class,'_9407 _5dba')])[2]"));
+		Select s2=new Select(dropdown2);
+		s2.selectByVisibleText("Oct");	
+	}
+	@Test
+	private void testCase7() {
+		WebElement dropdown3 = driver.findElement(By.xpath("(//select[contains(@class,'_9407 _5dba')])[3]"));
+		Select s3=new Select(dropdown3);
+		s3.selectByVisibleText("2018");
 	}
 	@AfterMethod
 	public void currenUrl() {
