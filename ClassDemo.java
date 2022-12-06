1dffa6a2c1a4a7930d7a20e33705684cd8c68187
Durairaji <112107468+Durairaji@users.noreply.github.com>
2022-12-06 09:06:24 +0530

    moreThanOneTestCase

--- ClassDemo.java
+++ ClassDemo.java
@@ -14,6 +14,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.itextpdf.text.pdf.BarcodeEAN;
+
 public class ClassDemo {
 	WebDriver driver;
 	@BeforeClass
@@ -24,7 +26,7 @@ public class ClassDemo {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.manage().window().maximize();
 		}
-	@BeforeMethod
+		@BeforeMethod
 	public void browserLaunch() {
 	driver.get("https://www.facebook.com/campaign/landing.php?campaign_id=14884913640&extra_1=s%7Cc%7C550525805640%7Cb%7Cfb%20create%20account%7C&placement=&creative=550525805640&keyword=fb%20create%20account&partner_id=googlesem&extra_2=campaignid%3D14884913640%26adgroupid%3D128696221432%26matchtype%3Db%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-332264388364%26loc_physical_ms%3D1007809%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=CjwKCAiAp7GcBhA0EiwA9U0mthjBxl43F-J6cdpIUnSCYQxvXF2DXpPNWzJ624RVPJ9NhRCEiQcHQhoCwKkQAvD_BwE");	
 	
@@ -73,6 +75,15 @@ public class ClassDemo {
 		Select s3=new Select(dropdown3);
 		s3.selectByVisibleText("2018");
 	}
+	@Test
+	private void testCase8() {
+		WebElement radio = driver.findElement(By.xpath("//label[text()='Male']"));
+		radio.click();
+	}
+	@Test
+	private void testCase9() {
+		driver.findElement(By.xpath("(//button[@type='submit'])[1]")).click();
+	}
 	@AfterMethod
 	public void currenUrl() {
 		System.out.println(driver.getCurrentUrl());
@@ -80,5 +91,6 @@ public class ClassDemo {
 	@AfterClass
 	public void quitPage() {
 		driver.quit();
+		
 	}
 }
