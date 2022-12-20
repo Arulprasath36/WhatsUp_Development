b6c365cfbd6ea3c8aca374d8065ffe03430ba07f
Durairaji <112107468+Durairaji@users.noreply.github.com>
2022-12-20 19:52:52 +0530

    LOG4J pratise

---
+++ Log4jTest.java
@@ -0,0 +1,23 @@
+package pageObjectModel3;
+
+import java.time.Duration;
+
+import org.apache.logging.log4j.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import com.itextpdf.text.pdf.BarcodeEAN;
+
+public class Log4jTest {
+	BarcodeEAN code = new BarcodeEAN();
+
+	public static void main(String[] arg) {
+		Logger demologger = LogManager.getLogger(Log4jTest.class.getName());
+		demologger.info("this is info statement");
+		demologger.error("this is error statement");
+		demologger.debug("this is debug statement");
+		demologger.fatal("this is fatal statement");
+
+	}
+}
